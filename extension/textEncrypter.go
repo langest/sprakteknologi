49a8986812a490eb5978dfab44a841c1a2a58187
Daniel Langesten <daniel.langest@gmail.com>
2014-12-18 10:02:27 +0100

    started working on neighbours

--- extension/textEncrypter.go
+++ extension/textEncrypter.go
@@ -1,17 +1,55 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	neighbours map[string][]string
+)
+
 func main() {
+	initNeighbours()
 	lines, err := readLines("toEncrypt.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	var string []ncLines
+	//var encLines []string
 	for _, line := range lines {
-		words.split
+		for _, c := range line {
+			if c == ' ' {
+				fmt.Println(c)
+			} else {
+				fmt.Println(neighbours[fmt.Sprintf("%c", c)])
+			}
+		}
 	}
 
+}
+
+func initNeighbours() {
+	neighbours = make(map[string][]string)
+	neighbours["a"] = []string{"q", "w", "s", "x", "z"}
+	neighbours["b"] = []string{"v", "g", "h", "n"}
+	neighbours["c"] = []string{"x", "f", "d", "v"}
+	neighbours["d"] = []string{"e", "r", "f", "c", "x", "x", "s"}
+	neighbours["e"] = []string{"w", "s", "d", "f", "r"}
+	neighbours["f"] = []string{"r", "d", "c", "v", "g", "t"}
+	neighbours["g"] = []string{"t", "y", "h", "b", "v", "f"}
+	neighbours["h"] = []string{"y", "u", "j", "n", "b", "g"}
+	neighbours["i"] = []string{"u", "j", "k", "l", "o"}
+	neighbours["j"] = []string{"u", "i", "k", "m", "n", "h"}
+	neighbours["k"] = []string{"i", "o", "l", "m", "j"}
+	neighbours["m"] = []string{"n", "j", "k"}
+	neighbours["n"] = []string{"b", "h", "j", "m"}
+	neighbours["o"] = []string{"i", "k", "l", "p"}
+	neighbours["p"] = []string{"o", "l", "ö", "ä", "å"}
+	neighbours["q"] = []string{"a", "s", "w"}
+	neighbours["r"] = []string{"e", "d", "f", "g", "t"}
 }
 
 func readLines(path string) ([]string, error) {
